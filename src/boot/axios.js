f1353cca9463f3fe5e4c9ce65af14252b7ed7904
ljs <lenon461@gmail.com>
2020-03-06 00:45:34 +0900

    server endpoint

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
 
-const server = "http://127.0.0.1:6004";
+const server = "http://34.97.145.153:6004";
 axios.defaults.baseURL = server;
 
 Vue.prototype.$axios = axios;
